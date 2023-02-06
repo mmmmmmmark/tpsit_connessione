5777131436a3150a3d15add814b6fd7744e7d11e
mmmmmmmark <73381562+mmmmmmmark@users.noreply.github.com>
2023-02-06 13:13:05 +0100

    Update MainClient.java

--- MainClient.java
+++ MainClient.java
@@ -1,5 +1,10 @@
-class MainClient {
-  public static void main(String[] args) {
-    
-  }
+public class MainClient {
+
+public static void main(String[] args) {
+Client c1 = new Client("C1", "\u001b[34;1m");
+c1.connetti("localhost", 2000);
+c1.scrivi();
+c1.chiudi();
+}
+
 }
