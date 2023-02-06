8c0e725eaa265805893ef4c0d77b99ecf9cef2f0
mmmmmmmark <73381562+mmmmmmmark@users.noreply.github.com>
2023-02-06 13:13:27 +0100

    Update MainServer.java

--- MainServer.java
+++ MainServer.java
@@ -8,9 +8,20 @@ import java.util.logging.Logger;
 class MainServer  {
   public static void main(String[] args) {
     // abbiamo utilizzato come punto di riferimento il codice sorgente datoci dalla professoressa. stiamo creando degli oggetti di classe socket a cui avvieremo dei metodi speciali di quella particolare classe. abbiamo inserito delle librerie apposite e stiamo a verificare se esse presentano delle eccezioni particolari.
-    ServerSocket serverSocket = new serverSocket(2000);
-    System.out.println("IN ATTESA...");
-    Socket socket = serverSocket.accept();
-    Socket socket = serverSocket.close();
+    try{
+      ServerSocket serverSocket = new ServerSocket(2000);
+          
+    if(serverSocket!=null)
+    {
+       System.out.println("IN ATTESA...");
+    serverSocket.accept();
+      serverSocket.close();
+    }
+   
+    } catch(IOException EX){
+      System.out.println(EX);
+    }
+
+    
   }
 }
